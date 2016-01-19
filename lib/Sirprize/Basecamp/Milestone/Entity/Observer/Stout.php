7f07fb38526ac403e691eed66b1bf00894e4f4e7
Daniel Hemmerich <hemmerich@users.noreply.github.com>
2016-01-18 19:43:43 -0500

    Update Stout.php

--- lib/Sirprize/Basecamp/Milestone/Entity/Observer/Stout.php
+++ lib/Sirprize/Basecamp/Milestone/Entity/Observer/Stout.php
@@ -19,52 +19,52 @@ class Stout extends Abstrakt
 
     public function onCompleteSuccess(Entity $milestone)
     {
-        print $this->_getOnCompleteSuccessMessage($milestone)."\n";
+//        print $this->_getOnCompleteSuccessMessage($milestone)."\n";
     }
 
     public function onUncompleteSuccess(Entity $milestone)
     {
-        print $this->_getOnUncompleteSuccessMessage($milestone)."\n";
+//        print $this->_getOnUncompleteSuccessMessage($milestone)."\n";
     }
 
     public function onCreateSuccess(Entity $milestone)
     {
-        print $this->_getOnCreateSuccessMessage($milestone)."\n";
+//        print $this->_getOnCreateSuccessMessage($milestone)."\n";
     }
 
     public function onUpdateSuccess(Entity $milestone)
     {
-        print $this->_getOnUpdateSuccessMessage($milestone)."\n";
+//        print $this->_getOnUpdateSuccessMessage($milestone)."\n";
     }
 
     public function onDeleteSuccess(Entity $milestone)
     {
-        print $this->_getOnDeleteSuccessMessage($milestone)."\n";
+//        print $this->_getOnDeleteSuccessMessage($milestone)."\n";
     }
 
     public function onCompleteError(Entity $milestone)
     {
-        print $this->_getOnCompleteErrorMessage($milestone)."\n";
+//        print $this->_getOnCompleteErrorMessage($milestone)."\n";
     }
 
     public function onUncompleteError(Entity $milestone)
     {
-        print $this->_getOnUncompleteErrorMessage($milestone)."\n";
+//        print $this->_getOnUncompleteErrorMessage($milestone)."\n";
     }
 
     public function onCreateError(Entity $milestone)
     {
-        print $this->_getOnCreateErrorMessage($milestone)."\n";
+//        print $this->_getOnCreateErrorMessage($milestone)."\n";
     }
 
     public function onUpdateError(Entity $milestone)
     {
-        print $this->_getOnUpdateErrorMessage($milestone)."\n";
+//        print $this->_getOnUpdateErrorMessage($milestone)."\n";
     }
 
     public function onDeleteError(Entity $milestone)
     {
-        print $this->_getOnDeleteErrorMessage($milestone)."\n";
+//        print $this->_getOnDeleteErrorMessage($milestone)."\n";
     }
 
 }
