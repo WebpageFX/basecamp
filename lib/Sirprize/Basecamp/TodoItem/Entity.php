0c4c181e3199d54a87196f5672e9258ff04c8ee5
Daniel Hemmerich <hemmerich@users.noreply.github.com>
2017-08-16 15:49:35 -0400

    Update the todo list ID if it exists

--- lib/Sirprize/Basecamp/TodoItem/Entity.php
+++ lib/Sirprize/Basecamp/TodoItem/Entity.php
@@ -318,6 +318,11 @@ class Entity
             $xml .= '<due-at>'.$this->getDueAt().'</due-at>';
         }
 
+        if ($this->_TODOLIST_ID !== null)
+        {
+                $xml .= '<todo-list-id type="integer">' . $this->_TODOLIST_ID . '</todo-list-id>';
+        }
+        
         $xml .= '</todo-item>';
         return $xml;
     }
